4ba728b0ec8d81d6ddeb7e480b13dd910db2424b
Md Mahtab Ansari <m.a.raj58232@gmail.com>
2025-08-31 18:00:00 +0530

    refactor: Improve code formatting and organization in ApplicationPage component

--- src/components/pages/application-page.tsx
+++ src/components/pages/application-page.tsx
@@ -1,25 +1,39 @@
-import { ApplicationDetails,ApplicationDetailsSkeleton } from '@/components/application';
+import { ApplicationDetails, ApplicationDetailsSkeleton } from '@/components/application';
 import { getUserApplication } from '@/lib/api/application';
-import { useEffect,useState,useCallback } from 'react';
-import type { ApplicationSchema} from '@/lib/schemas/application.schema';
+import { useEffect, useState, useCallback } from 'react';
+import type { ApplicationSchema } from '@/lib/schemas/application.schema';
 import { toast } from 'sonner';
 import { ResourceNotFound } from '@/components/resource-not-found';
+import { isUserLoggedIn } from "@/lib/api/auth"
+import { useNavigate } from "react-router-dom";
 
 export function ApplicationPage() {
     const [applicationData, setApplicationData] = useState<ApplicationSchema | null>(null);
     const [loading, setLoading] = useState<boolean>(true);
     const [notFound, setNotFound] = useState<boolean>(false);
+    const navigate = useNavigate();
+
+    const checkUserLoggedIn = useCallback(async () => {
+        const response = await isUserLoggedIn();
+        if (response.status !== "success") {
+            navigate("/");
+        }
+    }, [navigate]);
+
+    useEffect(() => {
+        checkUserLoggedIn();
+    }, [checkUserLoggedIn]);
 
     const fetchApplicationData = useCallback(async () => {
         setLoading(true);
         const response = await getUserApplication();
         if (response.status === "success") {
             setApplicationData(response.data);
-        }else{
-            if(response.statusCode === 404){
+        } else {
+            if (response.statusCode === 404) {
                 setNotFound(true);
             }
-            else{
+            else {
                 toast.error(response.message || "Failed to fetch application data");
             }
         }
@@ -29,6 +43,8 @@ export function ApplicationPage() {
     useEffect(() => {
         fetchApplicationData();
     }, [fetchApplicationData]);
+
+    
 
     if (loading) {
         return <ApplicationDetailsSkeleton />;
